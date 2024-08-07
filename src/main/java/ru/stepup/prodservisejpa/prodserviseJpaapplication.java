cb1ca6db118034f4ea80051a74e3c7e5b5623137
GSHishkin <glebshishkin@mail.ru>
2024-08-07 11:46:14 +0300

    First commit

---
+++ src/main/java/ru/stepup/prodservisejpa/prodserviseJpaapplication.java
@@ -0,0 +1,13 @@
+package ru.stepup.prodservisejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class prodserviseJpaapplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(prodserviseJpaapplication.class, args);
+	}
+
+}
